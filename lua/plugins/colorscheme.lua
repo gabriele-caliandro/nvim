6c33fe4fb6fd7aca08be98dcbe5718a061471982
gcaliandro@gmail.com <gcaliandro@gmail.com>
2025-07-28 19:08:17 +0200

    feat: added vs-code colorscheme, added VimBeGood, added jk keymap to escape insert mode

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "Mofiqul/vscode.nvim",
+    opts = {
+      style = "dark", -- This matches VS Code Dark Modern
+      transparent = false,
+      italic_comments = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,9 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+  "ThePrimeagen/vim-be-good",
+  cmd = "VimBeGood", -- Lazy load on command
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
