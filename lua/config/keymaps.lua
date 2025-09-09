f49e3dbbd768e2a271647c069ae884fba0e9faca
Gabriele Caliandro <gabrielecaliandro2001@gmail.com>
2025-09-09 14:26:40 +0200

    added usefull keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,11 +3,27 @@
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<esc>", { desc = "Exit insert mode" })
 
--- Terminal mappings
-vim.keymap.set("n", "<leader>tv", function()
-  vim.cmd("vsplit | terminal")
-end, { desc = "Vertical Terminal" })
+-- Visual mode surround shortcuts - press character to wrap selection
+-- These work by triggering mini.surround's visual mode add function
+vim.keymap.set("v", "'", "gsa'", { desc = "Surround with single quotes", remap = true })
+vim.keymap.set("v", '"', 'gsa"', { desc = "Surround with double quotes", remap = true })
+vim.keymap.set("v", "(", "gsa)", { desc = "Surround with parentheses", remap = true })
+vim.keymap.set("v", "[", "gsa]", { desc = "Surround with brackets", remap = true })
+vim.keymap.set("v", "{", "gsa}", { desc = "Surround with braces", remap = true })
+vim.keymap.set("v", "<", "gsa>", { desc = "Surround with angle brackets", remap = true })
 
-vim.keymap.set("n", "<leader>th", function()
-  vim.cmd("split | terminal")
-end, { desc = "Horizontal Terminal" })
+-- Select all (like Ctrl+A in other editors)
+vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
+
+-- Duplicate line up/down (like Shift+Alt+Up/Down in VSCode)
+vim.keymap.set("n", "<A-S-k>", "yyP", { desc = "Duplicate line up" })
+vim.keymap.set("n", "<A-S-j>", "yyp", { desc = "Duplicate line down" })
+vim.keymap.set("v", "<A-S-k>", "y'<P", { desc = "Duplicate selection up" })
+vim.keymap.set("v", "<A-S-j>", "y'>p", { desc = "Duplicate selection down" })
+
+-- Center screen after jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up and center" })
+
+-- Replace selection without losing clipboard content
+vim.keymap.set("v", "p", '"_dP', { desc = "Paste without losing clipboard" })
