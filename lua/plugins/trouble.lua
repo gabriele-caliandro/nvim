c4ebd9f9992b29857d838462a9332d64dc8afcd0
Gabriele Caliandro <gcaliandro@dymation.com>
2025-11-18 11:20:40 +0100

    updated dep

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    modes = {
+      diagnostics = {
+        position = "right",
+        -- Optional: adjust width
+        -- size = { width = 0.3 }, -- 30% of screen width
+      },
+      todo = {
+        position = "top",
+      },
+    },
+  },
+}
