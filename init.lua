6bf30e4a9e531c8bc6bab778e4b9f8d86404e825
gabriele-caliandro <gabrielecaliandro2001@gmail.com>
2025-12-25 13:35:46 +0100

    feat: added OC 52 support to be able to use copy and paste with clipboard featuers

--- init.lua
+++ init.lua
@@ -1,2 +1,22 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
+
